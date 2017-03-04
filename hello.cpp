56ee7517fd3048a5197ccb325e865a2072079fe3
Jeriah <jeriahyu@gmail.com>
2017-03-04 14:25:46 +0000

    Maybe the c script will actually be uploaded this time

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int favorite;
+    std::cout << "Hello, what is your favorite number?\n";
+    std::cin >> favorite;
+    std::cout << "Cool, my favorite number is also " << favorite;
+    return 0;
+}
