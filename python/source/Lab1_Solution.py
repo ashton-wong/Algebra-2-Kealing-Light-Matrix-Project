40e994cbda90434b1b2659af7b21c309f1e199f8
jyu78749 <jeriahyu@gmail.com>
2017-03-06 16:03:22 -0600

    [skip ci]

---
+++ python/source/Lab1_Solution.py
@@ -0,0 +1,25 @@
+############################################################
+# COSC 1336  Lab 1                                         #
+# Lab 1 Solution                                           #
+# This program displays a message with asterisk boarders.  #
+# Your program may not have had a main function. Starting  #
+# with lab 2 you will have a main function and have the    #
+# call to main at the end of your file. See any of the     #
+# program examples in your book starting in chapter 5 as   #
+# a guide.                                                 #
+############################################################
+
+def main():
+
+    # Display a line of asterisks
+    print("\n******************************************")
+
+    # Display message
+    print("* Don't cheat yourself, treat yourself. *")
+
+    # Display a line of asterisks
+    print("******************************************")
+
+
+# Call the main
+main() 
