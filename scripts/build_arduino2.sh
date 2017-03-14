d850b55bfdee493e7fbf87894e48ead473c0e034
jyu78749 <jeriahyu@gmail.com>
2017-03-14 11:49:11 -0500

    Delete build_arduino2.sh

--- scripts/build_arduino2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in arduino/main/*
-do
-  echo "Building $f"
-  platformio ci --lib=Algebra-2-Kealing-Light-Matrix-Project/arduino/refs/* --lib=Algebra-2-Kealing-Light-Matrix-Project/arduino/source/* --board=uno $f
-done
