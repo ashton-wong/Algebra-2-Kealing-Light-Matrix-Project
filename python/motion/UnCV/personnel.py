4666545531e0627fd1f0be14c7630468b3fb0bc9
Jeriah Yu <jeriahyu@gmail.com>
2017-05-14 09:48:29 -0500

    Added an easter egg, and continuing to modify the code.
    Signed-off-by: Jeriah Yu <jeriahyu@gmail.com>

--- python/motion/UnCV/personnel.py
+++ python/motion/UnCV/personnel.py
@@ -6,18 +6,23 @@ from PIL import Image
 import os
 import time
 
+#Used for video creation
+import cv2
+
 #The bulk of the analysis goes here!
 class EntityCapture(PiMotionAnalysis):
-    #QUEUE_SIZE = 10 # the number of consecutive frames to analyze
-    #THRESHOLD = 1.0 # the minimum average motion required in either axis
 
     def __init__(self, camera):
         super(EntityCapture, self).__init__(camera)
         self.frame_num = 0
-        #self.x_queue = np.zeros(self.QUEUE_SIZE, dtype=np.float)
-        #self.y_queue = np.zeros(self.QUEUE_SIZE, dtype=np.float)
+
+        #If anyone has time and wants to figure this out, go ahead
+        #Creates a video file called test with whatever codec chosen
+        #You will have to rename the video with the right extension
+        #self.video = cv2.VideoWriter("test.mp4", -1, 24, (640, 480))
 
     def analyze(self, a):
+        #Distance formula :)
         data = np.sqrt(
                 np.square(a['x'].astype(np.float)) +
                 np.square(a['y'].astype(np.float))
@@ -25,37 +30,32 @@ class EntityCapture(PiMotionAnalysis):
         img = Image.fromarray(data)
 
         #Save frame images
-        #filename = 'frame' + str(self.frame_num) + '.png'
-        #print('Writing', filename)
-        #img.save(filename)
-        #self.frame_num += 1
+        filename = 'frame%03d.png' % self.frame_num
+        print(filename)
+        img.save(filename)
+        self.frame_num += 1
 
-        #Display to screen (not quite working)
-        img.show()
-    
-        # Roll the queues and overwrite the first element with a new
-        # mean (equivalent to pop and append, but faster)
-        #self.x_queue[1:] = self.x_queue[:-1]
-        #self.y_queue[1:] = self.y_queue[:-1]
-        #self.x_queue[0] = a['x'].mean()
-        #self.y_queue[0] = a['y'].mean()
-        # Calculate the mean of both queues
-        #x_mean = self.x_queue.mean()
-        #y_mean = self.y_queue.mean()
-        # Convert left/up to -1, right/down to 1, and movement below
-        # the threshold to 0
-        #if abs(x_mean) < self.THRESHOLD:
-        #    x_move = ''
-        #elif x_mean < 0.0:
+        #Straight to a video
+        #self.video.write(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
 
+    #def finish(self):
+        #self.video.release()
 
 #This part must go at the end.
 #Keeps the program going...until Ctrl-C
 with picamera.PiCamera(resolution='VGA', framerate=24) as camera:
     with EntityCapture(camera) as detector:
         camera.rotation = 180
-        
+
+        print("Warming up...")
         time.sleep(2)
+
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
 
         camera.start_recording(
             os.devnull, format='h264', motion_output=detector)
@@ -68,3 +68,4 @@ with picamera.PiCamera(resolution='VGA', framerate=24) as camera:
             pass
         finally:
             camera.stop_recording()
+            #detector.finish()
