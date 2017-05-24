1b09884222ea5c2daa04771d910c943e6447ecc5
AlexY <alex@yang2k.com>
2017-05-24 10:27:38 -0500

    Update personnel.py

--- python/motion/UnCV/personnel.py
+++ python/motion/UnCV/personnel.py
@@ -5,6 +5,7 @@ from picamera.array import PiMotionAnalysis
 from PIL import Image
 import os
 import time
+import serial
 
 #Used for video creation
 #import cv2
@@ -15,6 +16,8 @@ class EntityCapture(PiMotionAnalysis):
     def __init__(self, camera):
         super(EntityCapture, self).__init__(camera)
         self.frame_num = 0
+        self.boundfail = 0
+        self.persist = np.zeros((26, 45))
 
         #If anyone has time and wants to figure this out, go ahead
         #Creates a video file called test with whatever codec chosen
@@ -28,31 +31,104 @@ class EntityCapture(PiMotionAnalysis):
         #Make a new array or change data so that it shows
         #what's in front of the camera
 
-        #So this is just magnitude of change (distance) turned into a
-        #picture! NOT WHAT WE WANT!
-        #data = np.sqrt(
-                #np.square(a['x'].astype(np.float)) +
-                #np.square(a['y'].astype(np.float))
-                #).clip(0, 255).astype(np.uint8)
-        #img = Image.fromarray(data)
+        #print(a['x'].shape)
+        #print(a['x'])
         
+        #print(a['y'].shape)
+        #print(a['y'])
 
-        #Frame metadata
-        filename = 'frame%03d.png' % self.frame_num
-        print(filename)
+        #print(a['sad'].shape)
+        #print(a['sad'])
+
+        magnitude = np.sqrt(
+                np.square(a['x'].astype(np.float)) +
+                np.square(a['y'].astype(np.float))
+                ).astype(np.uint8)
 
         #Debug
         #print(data.shape[0])
         #print(data.shape[1]-1)
         #print(a['x'])
         #print(a['y'])
+
+        x_index, y_index = np.where(magnitude != 0
+
+        filename = 'frame%03d.png' % self.frame_num
+        print(filename)
+        #print(x_index)
+        #print(y_index)
+
+        for index in range(x_index.shape[0]):
+            ser1.write(chr(x_index[index]))
+            ser1.write(chr(y_index[index]))
+            #ser2.write(chr(x_index[index]))
+            #ser2.write(chr(y_index[index]))
+
+        #End of new stuff
         
-        x = np.where(a['x'] != 0)
-        print(x)
+        #for i in range(magnitude.shape[0]):
+            #for j in range(magnitude.shape[1]):
+                #if magnitude[i][j] != 0:
+                    #self.persist[i][j] = 1
+                    #value_x = a[i][j][0]
+                    #value_y = a[i][j][1]
+                    #prev_x = i - int(value_x / 16)
+                    #prev_y = j - int(value_y / 16)
+                    #if (prev_x < 30) and (prev_y < 41) and (prev_x >= 0) and (prev_y >= 0):
+                        #self.persist[prev_x][prev_y] = 0
+                    #else:
+                        #print("Out of bounds:", prev_x, prev_y)
+
+        #if x_index != []:
+        #    cur_index = 0
+        #    for i in np.nditer(x_index):
+        #        j = y_index[cur_index]
+        #        value_x = a[i][j][0]
+        #        value_y = a[i][j][1]
+                #print("x index:", i)
+                #print("x value:", value_x)
+                #print("x block:", int(value_x / 16))
+                #print("y index:", j)
+                #print("y value:", value_y)
+                #print("y block:", int(value_y / 16))
+                #print("--------------------------")
+                #self.persist[i][j] = magnitude[i][j]
+                #try:
+                #    self.persist[i + int(value_x / 16)][j + int(value_y / 16)] = 0
+                #except IndexError:
+                #    self.boundfail += 1
+                #cur_index += 1
+
+        #h, i = np.where(a['x'] != 0)
+        #v_x = a['x'][h][i]
+        #print(h, v_x)
         
-        y = np.where(a['y'] != 0)
-        print(y)                
-            
+        #j, k = np.where(a['y'] != 0)
+        #v_y = a['y'][j][k]
+        #print(k, v_y)
+
+        #Frame metadata
+        #print(x_index)
+        #print(y_index)
+
+        #viewable = np.multiply(self.persist, 255)
+
+        #img = Image.fromarray(viewable.clip(0, 255).astype(np.uint8))
+
+        #for i in range(self.persist.shape[0]):
+            #for j in range(self.persist.shape[1]):
+                #if self.persist[i][j] == 1:
+                    #print("1", end='')
+                #else:
+                    #print("0", end='')
+            #print()ser = serial.Serial('/dev/ttyACM0', 115200)
+        #print("-----------------------------------------------------------")
+        #print(self.persist.shape[0], self.persist.shape[1])
+        
+        #print(viewable)
+        #print(magnitude)
+        #print(self.persist)
+                
         #Save frames
         #img.save(filename)
         self.frame_num += 1
@@ -62,15 +138,18 @@ class EntityCapture(PiMotionAnalysis):
         #self.video.write(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
 
     def finish(self):
+                                    
         self.video.release()
 
 #This part must go at the end.
 #Keeps the program going...until Ctrl-C
-with picamera.PiCamera(resolution='VGA', framerate=24) as camera:
+with picamera.PiCamera(resolution=(416, 704), framerate=24) as camera:
     with EntityCapture(camera) as detector:
         camera.rotation = 180
 
         print("Warming up...")
+        ser1 = serial.Serial('/dev/ttyACM0', 115200)
+        #ser2 = serial.Serial('/dev/ttyACM1', 115200)
         time.sleep(2)
 
         print("3")
@@ -92,4 +171,5 @@ with picamera.PiCamera(resolution='VGA', framerate=24) as camera:
             pass
         finally:
             camera.stop_recording()
+            #print("Out of ranges:", detector.boundfail)
             #detector.finish()
