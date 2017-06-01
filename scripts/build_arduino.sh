a94bb9431d64235e35065c36c35fc92789c5e3d7
jyu78749 <jeriahyu@gmail.com>
2017-06-01 10:43:33 -0500

    Update build_arduino.sh
    Tests Alan's code for errors ;) in main

--- scripts/build_arduino.sh
+++ scripts/build_arduino.sh
@@ -7,3 +7,8 @@ do
   echo "Building $f"
   platformio ci --lib=arduino/refs/* --lib=arduino/source/* --board=uno $f
 done
+
+for q in main/Final_Plain_Rain/*
+do
+  platformio ci --lib=arduino/refs/* --board=uno $q
+done
